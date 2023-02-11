416ce0cf55e56be847606c46b0099a2559a67e34
Abel Murgas <abelmurgasdev@gmail.com>
2023-02-11 14:01:22 -0500

    add while loop

--- 02_Module/03_while_loop.py
+++ 02_Module/03_while_loop.py
@@ -12,10 +12,38 @@
 #     i = i + 1
 
 # In Python, a while loop may have an optional else block.
-counter = 0
+# counter = 0
 
-while counter < 3:
-    print('Inside loop')
-    counter = counter + 1
-else:
-    print('Inside else')
+# while counter < 3:
+#     print('Inside loop')
+#     counter = counter + 1
+# else:
+#     print('Inside else')
+
+# --- BREAK ---
+# The break statement is used to terminate the loop immediately when it is encountered.
+
+# Example:
+# c = 0
+# while (c<3): # This loop print (0, 1, 2)
+#     print(c)
+#     c+= 1
+
+# c = 0
+# while (c<3): # This loop print (0)
+#     print(c)
+#     break # Break the loop after print 0
+#     c+= 1
+
+# --- CONTINUE ---
+# The continue statement is used to skip the current iteration of the loop
+# and the control flow of the program goes to the next iteration.
+# c = 0
+# for i in range(3):# This loop print (0, 2)
+#     if (i == 1):
+#         continue # If i equal to 1 the for skip the next line(print)
+#     print(i)
+
+# --- PASS ---
+# In Python programming, the pass statement is a null statement 
+# which can be used as a placeholder for future code.
